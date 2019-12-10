1501f06e2b31beb71b25da62a12070c60c1de471
Vladimir Pasechnik <vladimir.pasechnik@gmail.com>
2019-12-10 21:28:39 +0100

    add docker deployment

--- src/models/category.js
+++ src/models/category.js
@@ -1,13 +1,18 @@
 import { obj } from 'the-utils'
+import * as faker from 'faker'
 
 export const fields = {
   id: 0,
   categoryId: null,
   name: '',
+  img: '',
   description: '',
 }
 
-export const cast = props => obj.cast(fields, props)
+export const cast = props => {
+  const onProps = { ...props, img: props.img === undefined || props.img.length === 0 ? faker.image.image() : props.img }
+  return obj.cast(fields, onProps)
+}
 export const output = props => cast(props)
 export const patch = (o, props) => obj.cast(o, props)
 export const filter = props => obj.createFilter(fields, props)
--- src/models/product.js
+++ src/models/product.js
@@ -1,3 +1,4 @@
+import * as faker from 'faker'
 import { obj } from 'the-utils'
 
 export const fields = {
@@ -5,10 +6,14 @@ export const fields = {
   name: '',
   categoryId: null,
   price: 0,
+  img: '',
   description: '',
 }
 
-export const cast = props => obj.cast(fields, props)
+export const cast = props => {
+  const onProps = { ...props, img: props.img === undefined || props.img.length === 0 ? faker.image.image() : props.img }
+  return obj.cast(fields, onProps)
+}
 export const output = props => cast(props)
 export const patch = (o, props) => obj.cast(o, props)
 export const filter = props => obj.createFilter(fields, props)
